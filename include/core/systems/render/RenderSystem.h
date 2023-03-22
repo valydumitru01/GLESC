46b3a8162171116f3101eaac1123857449ae6fe5
Valentin Dumitru <62762857+valydumitru01@users.noreply.github.com>
2023-03-22 21:42:03 +0100

    Changed stuff, not working

--- include/core/systems/render/RenderSystem.h
+++ include/core/systems/render/RenderSystem.h
@@ -15,95 +15,95 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include "core/components/RenderComponent.h"
-#include "CoordinateSystem.h"
+#include "core/systems/render/CoordinateSystem.h"
 #include "core/systems/render/shaders/ShaderManager.h"
 #include "TextureManager.h"
-#include "../Debugger.h"
+#include "core/Debugger.h"
 
 class RenderSystem : public System {
 public:
-	explicit RenderSystem(Window &window) : window(window), shaderManager(ShaderManager()) {
-		addComponentRequirement<TransformComponent>();
-		addComponentRequirement<RenderComponent>();
+    explicit RenderSystem(Window &window) : window(window), shaderManager() {
+        addComponentRequirement<TransformComponent>();
+        addComponentRequirement<RenderComponent>();
 
-	}
+    }
 
-	/**
-	 * @brief Render the content of the screen.
-	 * Must be called every frame
-	 *
-	 */
-	void update(const double timeOfFrame) {
+    /**
+     * @brief Render the content of the screen.
+     * Must be called every frame
+     *
+     */
+    void update(const double timeOfFrame) {
 
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		for (auto &entity: getAssociatedEntities()) {
-			{
-				auto &render = getComponent<RenderComponent>(entity);
+        for (auto &entity: getAssociatedEntities()) {
+            {
+                auto &render = getComponent<RenderComponent>(entity);
 
-				// Bind the vertex array and buffer objects
-				glBindVertexArray(render.VAO);
-				glCheckError();
-				glBindBuffer(GL_ARRAY_BUFFER, render.VBO);
-				glCheckError();
+                // Bind the vertex array and buffer objects
+                glBindVertexArray(render.VAO);
+                glCheckError();
+                glBindBuffer(GL_ARRAY_BUFFER, render.VBO);
+                glCheckError();
 
-				// Set the model matrix uniform
-				GLuint modelLoc = glGetUniformLocation(shaderManager.getShaderProgram(), "model");
-				glUniformMatrix4fv(int(modelLoc), 1, GL_FALSE, &render.model[0][0]);
+                // Set the model matrix uniform
+                GLuint modelLoc = glGetUniformLocation(shaderManager.getShaderProgram(), "model");
+                glUniformMatrix4fv(int(modelLoc), 1, GL_FALSE, &render.model[0][0]);
 
-				// Draw the vertices
-				glDrawArrays(GL_TRIANGLES, 0, 3);
-			}
-			//TODO: Batch rendering
-			window.swapBuffers();
-		}
-	}
+                // Draw the vertices
+                glDrawArrays(GL_TRIANGLES, 0, 3);
+            }
+            // TODO: Batch rendering
+            window.swapBuffers();
+        }
+    }
 
-	void setMvp(glm::mat4 &mvp_) {
-		this->mvp = mvp_;
-	}
+    void setMvp(glm::mat4 &mvp_) {
+        this->mvp = mvp_;
+    }
 
 private:
-	glm::mat4 mvp;
+    glm::mat4 mvp;
 
-	/**
-	 * @brief Create a Shader Attributes object
-	 *
-	 */
-	void createShaderAttributes() {
+    /**
+     * @brief Create a Shader Attributes object
+     *
+     */
+    void createShaderAttributes() {
 
-	}
+    }
 
-	/**
-	 * @brief Create VBO, VAO and EBO buffers
-	 *
-	 */
-	void createBuffers();
+    /**
+     * @brief Create VBO, VAO and EBO buffers
+     *
+     */
+    void createBuffers();
 
-	/**
-	 * @brief Generate a texture
-	 *
-	 * @param texture ID to the texture object
-	 * @param path
-	 */
-	void generateTexture(GLuint texture, const char *path);
+    /**
+     * @brief Generate a texture
+     *
+     * @param texture ID to the texture object
+     * @param path
+     */
+    void generateTexture(GLuint texture, const char *path);
 
-	/**
-	 * @brief Iterate over the textures and generate them calling generateTexture()
-	 *
-	 */
-	void generateTextures();
+    /**
+     * @brief Iterate over the textures and generate them calling generateTexture()
+     *
+     */
+    void generateTextures();
 
-	ShaderManager shaderManager;
-	Window &window;
-	std::unordered_map<GLuint, const char *> textures;
+    ShaderManager shaderManager;
+    Window &window;
+    std::unordered_map<GLuint, const char *> textures;
 
 
-	std::vector<float> vertices;
-	std::vector<int> indices;
+    std::vector<float> vertices;
+    std::vector<int> indices;
 
-	glm::mat4 trans;
+    glm::mat4 trans;
 
-	unsigned int transformLoc;
+    unsigned int transformLoc;
 
 };
