e925f17b1faeaa64c7bef966ccdc6db66461b309
Valentin <valydumitru01@yahoo.com>
2024-06-18 20:37:12 +0200

    README.md completed

---
+++ .idea/fileTemplates/includes/C File Header.h
@@ -0,0 +1,9 @@
+/**************************************************************************************************
+ * @file   ${FILENAME}
+ * @author $USER_NAME
+ * @date   ${YEAR}-${MONTH}-${DAY}
+ * @brief  Add description of this file if needed @TODO 
+ *
+ * Copyright (c) ${YEAR} $USER_NAME. Licensed under the MIT License.
+ * See LICENSE.txt in the project root for license information.
+ **************************************************************************************************/
---
+++ .idea/fileTemplates/internal/C++ Class Header.h
@@ -0,0 +1,15 @@
+#parse("C File Header.h")
+#pragma once
+
+class ${NAME} {
+public:
+    /**
+     * @brief Default constructor.
+     */
+    ${NAME}();
+
+protected:
+
+private:
+
+}; // class ${NAME}
